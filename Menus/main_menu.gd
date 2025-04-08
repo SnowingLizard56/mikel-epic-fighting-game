a347d95651ac509a6bff80f3d860e895f2d0302c
Ben <149030607+SidewaysFox@users.noreply.github.com>
2025-04-08 15:55:56 +1200

    ruleset editor progress
    still need to make it work

--- Menus/main_menu.gd
+++ Menus/main_menu.gd
@@ -15,6 +15,7 @@ var current_menu_selection: int = 0
 func _ready() -> void:
 	%Title.self_modulate.a = 0
 	$TitleScreen/AnyButton.hide()
+	$Menu.hide()
 	for i in 5:
 		menu_bg_colours.append(Color(randf(), randf(), randf(), 0.25))
 	var cur_sel: int = 0
@@ -88,6 +89,7 @@ func _process(delta: float) -> void:
 		$MenuOptions.offset.y = lerp($MenuOptions.offset.y, 1080.0, 0.1)
 		$Menu.offset.y = lerp($Menu.offset.y, 0.0, 0.1)
 		%ForegroundBox.self_modulate = lerp(%ForegroundBox.self_modulate, menu_bg_colours[main_selection + 1], 0.15)
+		$Menu.show()
 		for child in $Menu.get_children():
 			child.hide()
 		$Menu.get_child(main_selection).show()
@@ -95,10 +97,22 @@ func _process(delta: float) -> void:
 		if state == 3:
 			# Moving gamemode selection
 			for i in 3:
-				%FightRuleset.get_child(i + 2).position.x = lerp(%FightRuleset.get_child(i + 2).position.x, -880.0 + (Global.gamemode * 352) + (i * 352), 0.15)
-			# Moving lives numbers
+				%RulesetOptions.get_child(i + 1).position.x = lerp(%RulesetOptions.get_child(i + 2).position.x, -880.0 + (Global.gamemode * 352) + (i * 352), 0.15)
+			# Moving everything on Y axis
+			%RulesetOptions.position.y = lerp(%RulesetOptions.position.y, 300.0 - (current_menu_selection * 200), 0.15)
+			%RulesetOptions.position.y = clamp(%RulesetOptions.position.y, -1200.0, 0.0)
 			var n: int = 0
-			for child in %FightRuleset/LifeNumbers.get_children():
+			for child in %RulesetOptions.get_children():
+				child.self_modulate.a = sqrt(3.25 / (1 + (2.25 * (cos(deg_to_rad(child.position.x / 4))) ** 2))) * cos(deg_to_rad(child.position.x / 4))
+				child.self_modulate.a = clamp(child.self_modulate.a, 0.0, 1.0)
+				if child.position.y < 540 and child.position.y > -540:
+					child.show()
+				else:
+					child.hide()
+				n += 1
+			# Moving lives numbers
+			n = 0
+			for child in %RulesetOptions/LifeNumbers.get_children():
 				child.position.x = lerp(child.position.x, 176.0 - (Global.lives * 176) + (n * 176), 0.15)
 				child.self_modulate.a = sqrt(3.25 / (1 + (2.25 * (cos(deg_to_rad(child.position.x / 5))) ** 2))) * cos(deg_to_rad(child.position.x / 5))
 				child.self_modulate.a = clamp(child.self_modulate.a, 0.0, 1.0)
@@ -109,8 +123,19 @@ func _process(delta: float) -> void:
 				n += 1
 			# Moving starting damage numbers
 			n = 0
-			for child in %FightRuleset/StartDmgNumbers.get_children():
+			for child in %RulesetOptions/StartDmgNumbers.get_children():
 				child.position.x = lerp(child.position.x, 0.0 - ((Global.starting_damage / 10) * 176) + (n * 176), 0.15)
+				child.self_modulate.a = sqrt(3.25 / (1 + (2.25 * (cos(deg_to_rad(child.position.x / 5))) ** 2))) * cos(deg_to_rad(child.position.x / 5))
+				child.self_modulate.a = clamp(child.self_modulate.a, 0.0, 1.0)
+				if child.position.x < 800 and child.position.x > -800:
+					child.show()
+				else:
+					child.hide()
+				n += 1
+			# Moving damage multiplier numbers
+			n = 0
+			for child in %RulesetOptions/DmgMultNumbers.get_children():
+				child.position.x = lerp(child.position.x, 0.0 - ((Global.damage_multiplier * 4) * 176) + (n * 176), 0.15)
 				child.self_modulate.a = sqrt(3.25 / (1 + (2.25 * (cos(deg_to_rad(child.position.x / 5))) ** 2))) * cos(deg_to_rad(child.position.x / 5))
 				child.self_modulate.a = clamp(child.self_modulate.a, 0.0, 1.0)
 				if child.position.x < 800 and child.position.x > -800:
@@ -120,31 +145,41 @@ func _process(delta: float) -> void:
 				n += 1
 			# Gamemode select
 			if current_menu_selection == 0:
-				%FightRuleset/GamemodeShader.color = lerp(%FightRuleset/GamemodeShader.color, menu_bg_colours[main_selection + 1], 0.15)
+				%RulesetOptions/GamemodeShader.color = lerp(%RulesetOptions/GamemodeShader.color, menu_bg_colours[main_selection + 1], 0.15)
 				if Input.is_action_just_pressed("right") and Global.gamemode > 0:
 					Global.gamemode -= 1
 				if Input.is_action_just_pressed("left") and Global.gamemode < 2:
 					Global.gamemode += 1
 			else:
-				%FightRuleset/GamemodeShader.color = lerp(%FightRuleset/GamemodeShader.color, Color(1, 1, 1, 0.25), 0.15)
+				%RulesetOptions/GamemodeShader.color = lerp(%RulesetOptions/GamemodeShader.color, Color(1, 1, 1, 0.25), 0.15)
 			# Lives select
 			if current_menu_selection == 1:
-				%FightRuleset/LivesShader.color = lerp(%FightRuleset/LivesShader.color, menu_bg_colours[main_selection + 1], 0.15)
+				%RulesetOptions/LivesShader.color = lerp(%RulesetOptions/LivesShader.color, menu_bg_colours[main_selection + 1], 0.15)
 				if Input.is_action_just_pressed("right") and Global.lives < 10:
 					Global.lives += 1
 				if Input.is_action_just_pressed("left") and Global.lives > 1:
 					Global.lives -= 1
 			else:
-				%FightRuleset/LivesShader.color = lerp(%FightRuleset/LivesShader.color, Color(1, 1, 1, 0.25), 0.15)
+				%RulesetOptions/LivesShader.color = lerp(%RulesetOptions/LivesShader.color, Color(1, 1, 1, 0.25), 0.15)
 			# Start damage select
 			if current_menu_selection == 2:
-				%FightRuleset/StartDmgShader.color = lerp(%FightRuleset/StartDmgShader.color, menu_bg_colours[main_selection + 1], 0.15)
+				%RulesetOptions/StartDmgShader.color = lerp(%RulesetOptions/StartDmgShader.color, menu_bg_colours[main_selection + 1], 0.15)
 				if Input.is_action_just_pressed("right") and Global.starting_damage < 100.0:
 					Global.starting_damage += 10
 				if Input.is_action_just_pressed("left") and Global.starting_damage > 0.0:
 					Global.starting_damage -= 10
 			else:
-				%FightRuleset/StartDmgShader.color = lerp(%FightRuleset/StartDmgShader.color, Color(1, 1, 1, 0.25), 0.15)
+				%RulesetOptions/StartDmgShader.color = lerp(%RulesetOptions/StartDmgShader.color, Color(1, 1, 1, 0.25), 0.15)
+			# Start damage select
+			if current_menu_selection == 3:
+				%RulesetOptions/DmgMultShader.color = lerp(%RulesetOptions/DmgMultShader.color, menu_bg_colours[main_selection + 1], 0.15)
+				if Input.is_action_just_pressed("right") and Global.damage_multiplier < 5.0:
+					Global.damage_multiplier += 0.25
+				if Input.is_action_just_pressed("left") and Global.damage_multiplier > 0.0:
+					Global.damage_multiplier -= 0.25
+			else:
+				%RulesetOptions/DmgMultShader.color = lerp(%RulesetOptions/DmgMultShader.color, Color(1, 1, 1, 0.25), 0.15)
+			
 
 
 func _on_any_button_flicker_timeout() -> void:
--- global.gd
+++ global.gd
@@ -4,3 +4,4 @@ extends Node
 var gamemode: int = 1
 var lives: int = 3
 var starting_damage: float = 0.0
+var damage_multiplier: float = 1.0
